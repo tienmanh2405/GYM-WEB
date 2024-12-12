bd76bc9c4f2b698fe662a42dce2403f5c6b963a2
AquaSamaKwids <aquasangankyeam123@gmail.com>
2024-12-12 10:58:17 +0700

    sua routes

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 
 define('BASE_PATH', realpath(dirname(__FILE__) . '/../')); 
 
-require_once BASE_PATH . '/routes/router_ThietBi.php'; 
+require_once BASE_PATH . '/routes/router.php'; 
 
 $router = new Router();
 $router->handleRequest(); // Xử lý yêu cầu
--- routes/router.php
+++ routes/router.php
@@ -10,9 +10,13 @@ class Router {
     // Định nghĩa các route
     private function loadRoutes() {
         $this->routes = [
-            '' => ['controller' => 'HomeController_NV', 'action' => 'dashboard'],   
-            'NV_Quay/thanhVien' => ['controller' => 'HomeController_NV', 'action' => 'thanhVien'],     
-            'NV_Quay/thanhVien/chiTietThanhVien' => ['controller' => 'HomeController_NV', 'action' => 'chiTietThanhVien'],     
+            'NV_BaoTri/index' => ['controller' => 'HomeController_ThietBi', 'action' => 'index'],
+            'NV_BaoTri/thietBi' => ['controller' => 'HomeController_ThietBi', 'action' => 'showDevices'],
+            'NV_BaoTri/phieuBaoTri' => ['controller' => 'HomeController_ThietBi', 'action' => 'showPhieuBaoTri'], 
+            'NV_BaoTri/quanlykhuyenmai' => ['controller' => 'HomeController_KhuyenMai', 'action' => 'index'],
+            'NV_BaoTri/quanlykhuyenmai/update' => ['controller' => 'HomeController_KhuyenMai', 'action' => 'update'],
+            'NV_BaoTri/quanlykhuyenmai/create' => ['controller' => 'HomeController_KhuyenMai', 'action' => 'create'],
+            'NV_BaoTri/quanlykhuyenmai/delete/{maKhuyenMai}' => ['controller' => 'HomeController_KhuyenMai', 'action' => 'delete'], // Route cho xóa
         ];
     }
 
@@ -22,21 +26,32 @@ class Router {
         $path = str_replace($basePath, '', $_SERVER['REQUEST_URI']);
         $path = trim($path, '/');
         $path = parse_url($path, PHP_URL_PATH);
-        if (array_key_exists($path, $this->routes)) {
-            $controllerName = $this->routes[$path]['controller'];
-            $actionName = $this->routes[$path]['action'];
-            $this->dispatch($controllerName, $actionName);
+        
+        // Kiểm tra nếu có tham số cho xóa
+        if ($this->isDeleteRequest($path, $matches)) {
+            $this->dispatch('HomeController_KhuyenMai', 'delete', $matches[1]);
+        } elseif (array_key_exists($path, $this->routes)) {
+            $this->dispatch($this->routes[$path]['controller'], $this->routes[$path]['action']);
         } else {
             $this->dispatch('ErrorController', 'notFound'); // Trang lỗi 404
         }
     }
 
+    // Kiểm tra xem yêu cầu có phải là yêu cầu xóa không
+    private function isDeleteRequest($path, &$matches) {
+        return preg_match('/^NV_BaoTri\/quanlykhuyenmai\/delete\/(\w+)$/', $path, $matches); // Sử dụng \w+ để hỗ trợ cả chuỗi và số
+    }
+
     // Triển khai controller/action
-    private function dispatch($controllerName, $actionName) {
+    private function dispatch($controllerName, $actionName, $param = null) {
         require_once BASE_PATH . "/controllers/$controllerName.php";
         $controller = new $controllerName();
         if (method_exists($controller, $actionName)) {
-            $controller->$actionName();
+            if ($param) {
+                $controller->$actionName($param);
+            } else {
+                $controller->$actionName();
+            }
         } else {
             echo "Action $actionName không tồn tại.";
         }
